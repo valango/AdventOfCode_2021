1adc6060f7b78fd778c25906fb4281dfacb0639c
Villem Alango <villem.alango@gmail.com>
2021-12-06 08:54:13 +0200

    commented: day06

--- day06.js
+++ day06.js
@@ -2,34 +2,35 @@
 const rawInput = [require('./data/day06')]  //  Lanternfish
 const { parseInt } = require('./utils')
 
-//  Simple simulation, but runs out of memory for puzzle2.
-const simulate1 = (initialAges, dayCount) => {
-  const ages = initialAges.slice()
+//  Simple simulation, but slow, and runs out of memory for puzzle2.
+const simulate1 = (initialPopulation, dayCount) => {
+  const population = initialPopulation.slice()
 
   for (let day = 0; day < dayCount; ++day) {
-    for (let i = 0, age, limit = ages.length; i < limit; ages[i++] = age) {
-      if ((age = ages[i] - 1) < 0) {
-        ages.push(8)
-        age = 6
+    for (let i = 0, daysLeft, limit = population.length; i < limit; population[i++] = daysLeft) {
+      if ((daysLeft = population[i] - 1) < 0) {
+        population.push(8)
+        daysLeft = 6
       }
     }
   }
-  return ages.length
+  return population.length
 }
 
-//  A functional equivalent for simulation1, but much quicker and insensitive to parameters.
-const simulate2 = (initialAges, dayCount) => {
-  let countsByDaysLeft = [0,0,0,0,0,0,0,0,0]
+//  A functional equivalent for simulate1, but O(n) to dayCount.
+const simulate2 = (initialPopulation, dayCount) => {
+  let countsByDaysLeft = new Uint32Array(9)
 
-  for (const age of initialAges) {
-    countsByDaysLeft[age] += 1
+  for (const daysLeft of initialPopulation) {
+    countsByDaysLeft[daysLeft] += 1
   }
   for (let day = 0; day < dayCount; ++day) {
     const old = countsByDaysLeft
-    countsByDaysLeft = [0,0,0,0,0,0,0,0,0]
+
+    countsByDaysLeft = new Array(9)   //  Note: Uint32Array is unsafe here!
 
     for (let i = 0; i < 8; ++i) {
-      countsByDaysLeft[i] = old[i+1]
+      countsByDaysLeft[i] = old[i + 1]
     }
     countsByDaysLeft[6] += (countsByDaysLeft[8] = old[0])
   }
