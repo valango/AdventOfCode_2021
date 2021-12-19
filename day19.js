f52bd4c7b2fe93d407eabb5e7d4a32cca48e038c
Villem Alango <villem.alango@gmail.com>
2021-12-19 20:54:17 +0200

    added: core/arrayIndex

--- day19.js
+++ day19.js
@@ -1,89 +1,176 @@
-'use strict'
+'use strict'    //  day19 - interpreting scanner data.
 
+const { findRotation, getOffset, reverse, rotate, shift } = require('./day19rotations')
 const { assert, loadData, parseInt } = require('./core/utils')
-const permute = require('./core/permute')
-const rawInput = [loadData(module.filename, 'main'), loadData(module.filename, 'demo-tiny')]
-const { abs, cos, sin, PI } = Math
+const { intersection } = require('lodash')
+const rawInput = [loadData(module.filename), loadData(module.filename, 'demo')]
 
-/** @typedef {number[]} TPoint */
-/**
- * @typedef {{distances: number[][], points: TPoint[]}} TBlock
- *
- * distances entry: (distance, coordinateIndex0, indexA0, indexB0, coordinateIndex1, ...)
- */
-
-/** @return {TBlock[]} */
 const parse = (dsn) => {
   let input = rawInput[dsn]
   const data = []
 
   if (input && (input = input.split('\n'))) {
-    for (let distances, line, iLine = 0, iBlock = -1, point, points, r;
-         (line = input[iLine++]) !== undefined;) {
+    for (let line, iLine = 0, readings; (line = input[iLine++]) !== undefined;) {
       if (line) {
-        if ((r = /^---\sscanner\s(\d+)\s---$/i.exec(line))) {
-          ++iBlock
-          distances = [], points = []
-          data.push({ distances, points })
+        if (/^---\sscanner\s\d+\s---$/i.exec(line)) {
+          data.push(readings = [])
         } else {
-          point = line.split(',').map(parseInt)
-
-          for (let iR = 0, row; (row = points[iR]); ++iR) {
-            for (let iC = 0, distance, entry; iC < 3; ++iC) {
-              distance = abs(point[iC] - row[iC])
-
-              if (!(entry = distances.find(row => row[0] === distance))) {
-                distances.push(entry = [distance])
-              }
-              entry.push(iC, iR, points.length)
-            }
-          }
-          points.push(point)
+          readings.push(line.split(',').map(parseInt))
         }
       }
-    }
-    for (const { distances } of data) {
-      distances.sort((a, b) => b.length - a.length)
     }
     return data
   }
 }
 
-/*
-  Every scanner has 6 possible sight directions with 4 possible rotation angles each.
+/** @typedef {[number,number,number]} TPoint */
+
+/**
+ * @typedef {{
+ * diffs: TPoint[],
+ * pairs: [number, number][]
+ * scans: TPoint[]
+ * }} TBlock
  */
 
 /**
- * @param {TBlock[]} input
- * @param {TOptions} options
+ * @param {number}  x
+ * @param {number}  y
+ * @param {number}  z
+ * @param {TPoint}  pointB
+ * @return {TPoint} squared distances by all 3 projections
  */
-const puzzle1 = (input, options) => {
-  const { length } = input
+const getDistances = ([x, y, z], pointB) => {
+  x -= pointB[0], y -= pointB[1], z -= pointB[2]
+  x *= x, y *= y, z *= z
 
-  for (let blockA, blockB, iB = 0; (blockA = input[iB]); ++iB) {
-    const distsA = blockA.distances, pointsA = blockA.points
+  return [y + z, x + z, x + y]
+}
 
-    for (let iNext = iB; ++iNext < length;) {
-      let iCs = [0, 1, 2], entry
-      const { distances, points } = input[iB]
+const pointsDiff = ([a, b, c], [d, e, f]) => [a - d, b - e, c - f]
 
-      distsA.forEach((entryA, distA) => {
-        if ((entry = distances.get(distA))) {}
-      })
+/**
+ * @param {[number, number, number][]} pointReadings
+ * @return {TBlock}
+ */
+const composeBlock = (pointReadings) => {
+  const diffs = [], pairs = [], scans = pointReadings.slice()
+
+  for (let i = 0; i < scans.length; ++i) {
+    for (let j = 0; j < i; ++j) {
+      diffs.push(getDistances(pointReadings[i], pointReadings[j]))
+      pairs.push([j, i])
     }
   }
 
-  /* let m, r
-  m = createRotationMatrix([0, 0, 1])
-  r = rotate([1, 2, 3], m) */
-  return undefined
+  return { diffs, pairs, scans }
+}
+
+const doSetsMatch = (a, b) => {
+  let b1 = b.slice(0, 3), n = b1.length
+
+  for (let iA = 0, iB, vA; iA < 3; ++iA) {
+    if ((iB = b1.indexOf(a[iA])) >= 0 && iB < 3) {
+      b1[iB] = undefined, --n
+    }
+  }
+  return n === 0
+}
+
+/**
+ * @param {number} value
+ * @param {number[]} pairs
+ * @param {number} start
+ */
+const indexesOfPairsContaining = (value, pairs, start) => {
+  const result = []
+
+  for (let i = start; (i = pairs.indexOf(value, i)) >= 0; ++i) {
+    result.push(i >> 1)
+  }
+
+  return result
+}
+
+/**
+ * @param {TBlock} blockA
+ * @param {TBlock} blockB
+ */
+const findSimilarPairs = (blockA, blockB) => {
+  const indexesA = [], indexesB = [], idxA = [], idxB = []
+
+  for (let i = 0, dA; (dA = blockA.diffs[i]); i += 1) {
+    for (let j = 0, dB, v; (dB = blockB.diffs[j]); j += 1) {
+      if (doSetsMatch(dA, dB)) {
+        indexesA.push(...blockA.pairs[i])
+        indexesB.push(...blockB.pairs[j])
+      }
+    }
+  }
+  //  The best transform must have at least 12 fits.
+  for (let i = 0, indexA, indexB, a, b, c; (indexA = indexesA[i]) !== undefined; ++i) {
+    if (!idxA.includes(indexA)) {
+      if ((a = indexesOfPairsContaining(indexA, indexesA, i)).length > 1) {
+        b = c = undefined
+        if (!idxB.includes(indexB = indexesB[i])) {
+          if ((b = indexesOfPairsContaining(indexB, indexesB, 0)).length > 1) {
+            if (intersection(a, b).length > 1) {
+              idxA.push(indexA)
+              idxB.push(indexB)
+            }
+          }
+        }
+        if (!idxB.includes(indexB = indexesB[i ^ 1])) {
+          if ((b = indexesOfPairsContaining(indexB, indexesB, 0)).length > 1) {
+            if (intersection(a, b).length > 1) {
+              assert(!idxA.includes(indexA), 'inc')
+              idxA.push(indexA)
+              idxB.push(indexB)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  if (idxA.length >= 12) {
+    let r = findRotation(blockA.scans[idxA[0]], blockA.scans[idxA[1]],
+      blockB.scans[idxB[0]], blockB.scans[idxB[1]])
+    let q = findRotation(blockA.scans[idxA[1]], blockA.scans[idxA[2]],
+      blockB.scans[idxB[1]], blockB.scans[idxB[2]])
+    assert(r !== undefined)
+    assert(r === q)
+    let points = blockB.scans.map(p => rotate(p, r))
+    const offs = getOffset(points[idxB[0]], blockA.scans[idxA[0]])
+    points = points.map(p => shift(p, offs))
+    blockB.scans = points
+  }
+  return 0
+}
+
+/**
+ * @param {TPoint[][]} input - a block contains scans from one scanner.
+ */
+const solve1 = (input) => {
+  const blocks = input.map(composeBlock)
+
+  for (let i = 0; i < blocks.length; ++i) {
+    for (let j = 0; j < i; ++j) {
+      findSimilarPairs(blocks[i], blocks[j])
+    }
+  }
+  return 0
 }
 
 /**
  * @param {TPoint[][]} input
  * @param {TOptions} options
  */
-const puzzle2 = (input, options) => {
+const puzzle1 = (input, options) => {
+  return options && solve1(input)
+}
+
+const puzzle2 = () => {
   return undefined
 }
 
@@ -93,38 +180,3 @@ module.exports = { parse, puzzles: [puzzle1, puzzle2] }
  */
 
 /** @type Map<number,number[]> */
-const rotationMatrixCache = new Map()
-
-const createRotationMatrix = ([rx, ry, rz]) => {
-  assert(rx >= 0 && rx >= 0 && rx >= 0, 'createRotationMatrix')
-  //  https://en.wikipedia.org/wiki/Rotation_matrix
-  let result
-  const key = rx + ry << 2 + rz << 4
-
-  if ((result = rotationMatrixCache.get(key)) === undefined) {
-    const ax = Math.PI * rx / 2, ay = Math.PI * ry / 2, az = Math.PI * rz / 2
-    const cx = cos(ax), cy = cos(ay), cz = cos(az), sx = sin(ax), sy = sin(ay), sz = sin(az)
-
-    rotationMatrixCache.set(key, result = [
-      cx * cy, cx * sy * sz - sx * cz, cx * sy * cz + sx * sz,
-      sx * cy, sx * sy * sz + cx * cz, sx * sy * cz - cx * sz,
-      -sy, cy * sz, cy * cz
-    ])
-  }
-  return result
-}
-
-const rotate = (xyz, matrix) => {
-  const dim = 3, result = new Array(dim).fill(0)
-
-  for (let i = 0; i < dim; ++i) {
-    for (let ij, j = 0; j < dim; ++j) {
-      try {
-        result[i] += matrix[ij = i * dim + j] * xyz[j]
-      } catch (err) {
-        throw err
-      }
-    }
-  }
-  return result
-}
---
+++ day19rotations.js
@@ -0,0 +1,80 @@
+'use strict'
+const { assert } = require('./core/utils')
+
+const { abs, cos, sin } = Math
+
+const rotationMatrixCache = new Map()
+
+const codeToAngles = (code) => {
+  return [code & 3, (code >> 2) & 3, code >> 4 & 3]
+}
+
+const getRotationMatrix = (code) => {
+  assert(code >= 0 && code < 0b1000000 && code === Math.floor(code), 'getRotationMatrix')
+  //  https://en.wikipedia.org/wiki/Rotation_matrix
+  let result
+  const [rx, ry, rz] = codeToAngles(code)
+
+  if ((result = rotationMatrixCache.get(code)) === undefined) {
+    const ax = Math.PI * rx / 2, ay = Math.PI * ry / 2, az = Math.PI * rz / 2
+    const cx = cos(ax), cy = cos(ay), cz = cos(az), sx = sin(ax), sy = sin(ay), sz = sin(az)
+
+    rotationMatrixCache.set(code, result = [
+      cx * cy, cx * sy * sz - sx * cz, cx * sy * cz + sx * sz,
+      sx * cy, sx * sy * sz + cx * cz, sx * sy * cz - cx * sz,
+      -sy, cy * sz, cy * cz
+    ])
+  }
+  return result
+}
+
+const rotate = (xyz, matrix) => {
+  const dim = 3, result = new Array(dim).fill(0)
+
+  if (typeof matrix === 'number') matrix = getRotationMatrix(matrix)
+
+  for (let i = 0; i < dim; ++i) {
+    for (let ij, j = 0; j < dim; ++j) {
+      try {
+        result[i] += Math.round(matrix[ij = i * dim + j] * xyz[j])
+      } catch (err) {
+        throw err
+      }
+    }
+  }
+  return result
+}
+
+const findDiffRotations = (a, b) => {
+  const rots = []
+  for (let code = 0, c; code < 0b1000000; ++code) {
+    c = rotate(a, getRotationMatrix(code))
+    if (abs(c[0]) === b[0] && abs(c[1]) === b[1] && abs(c[2]) === b[2]) {
+      rots.push(code)
+    }
+  }
+  return rots
+}
+
+const findRotation = (a, b, c, d) => {
+  const dx = b[0] - a[0], dy = b[1] - a[1], dz = b[2] - a[2]
+  for (let code = 0, e, f; code < 0b1000000; ++code) {
+    e = rotate(c, getRotationMatrix(code))
+    f = rotate(d, getRotationMatrix(code))
+    if ((f[0] - e[0]) === dx && (f[1] - e[1]) === dy && (f[2] - e[2]) === dz) {
+      return code
+    }
+  }
+}
+
+const getOffset = ([x0, y0, z0], [x1, y1, z1]) => [x1 - x0, y1 - y0, z1 - z0]
+
+const shift = ([x, y, z], [dx, dy, dz]) => [x + dx, y + dy, z + dz]
+
+const reverse = ([x, y, z]) => [-z, -y, -z]
+
+for (let code = 0; code < 0b1000000; ++code) {
+  getRotationMatrix(code)
+}
+
+module.exports = { findDiffRotations, findRotation, getOffset, reverse, rotate, shift }
