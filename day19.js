f3ab7426fc6b54ed0ef1e41d7610b33839fd6367
Villem Alango <villem.alango@gmail.com>
2021-12-19 07:57:26 +0200

    day19 p1

--- day19.js
+++ day19.js
@@ -1,15 +1,30 @@
 'use strict'
 
 const { assert, loadData, parseInt } = require('./core/utils')
-const rawInput = [loadData(module.filename,'main'), loadData(module.filename,'demo'), 0, 0]
+const rawInput = [loadData(module.filename, 'main'), loadData(module.filename, 'demo-tiny')]
 
+/** @typedef {number[]} TPoint */
+
+/** @return {TPoint} */
 const parse = (dsn) => {
-  let data = rawInput[dsn]
+  let data = rawInput[dsn], line, lineNum, res, sectionNum, value
+  const scans = []
 
-  if (data && (data = data.split('\n').filter(v => Boolean(v))).length) {
-    return data
+  if (data && (data = data.split('\n'))) {
+    for (lineNum = 0, sectionNum = -1; (line = data[lineNum++]) !== undefined;) {
+      if (line) {
+        if ((res = /^---\sscanner\s(\d+)\s---$/i.exec(line))) {
+          ++sectionNum
+          // assert((value = parseInt(res[1])) === sectionNum, 'parse line #' + lineNum)
+          scans.push([])
+        } else {
+          scans[sectionNum].push(line.split(',').map(parseInt))
+        }
+      }
+    }
+    return scans
   }
-  return data   //  NOTE: The runner will distinguish between undefined and falsy!
+  return data
 }
 
 /**
@@ -27,11 +42,6 @@ const puzzle1 = (input, options) => {
 const puzzle2 = (input, options) => {
   return undefined
 }
-
-//  Example (demo) data.
-rawInput[1] = ``
-//  Uncomment the next line to disable demo for puzzle2 or to define different demo for it.
-//  rawInput[2] = ``
 
 module.exports = { parse, puzzles: [puzzle1, puzzle2] }
 
